1e5abcf41187bd6dc737f673b57c26310f970233
Aleksei Tiulpin <aleksei.tiulpin@protonmail.ch>
2018-07-24 15:21:43 +0300

    First version works for VGG16bn

--- bn_fusion.py
+++ bn_fusion.py
@@ -1,5 +1,54 @@
 import torch
-import torch.nn
+import torch.nn as nn
 
-def fuse_bn(model):
-    pass
+
+def fuse_bn_sequential(block):
+    """
+    This function takes a sequential block and fuses the batch normalization with convolution
+    :param model:
+    :return:
+    """
+    stack = []
+    for m in block.children():
+        if isinstance(m, nn.BatchNorm2d):
+            if isinstance(stack[-1], nn.Conv2d):
+                bn_st_dict = m.state_dict()
+                conv_st_dict = stack[-1].state_dict()
+
+                # BatchNorm params
+                eps = m.eps
+                mu = bn_st_dict['running_mean']
+                var = bn_st_dict['running_var']
+                gamma = bn_st_dict['weight']
+
+                if 'bias' in bn_st_dict:
+                    beta = bn_st_dict['bias']
+                else:
+                    beta = torch.zeros(gamma.size(0)).float().to(gamma.device)
+
+                # Conv params
+                W = conv_st_dict['weight']
+                if 'bias' in conv_st_dict:
+                    bias = conv_st_dict['bias']
+                else:
+                    bias = torch.zeros(W.size(0)).float().to(gamma.device)
+
+                denom = torch.sqrt(var + eps)
+                b = beta - gamma.mul(mu).div(denom)
+                A = gamma.div(denom)
+                bias *= A
+                A = A.expand_as(W.transpose(0, -1)).transpose(0, -1)
+
+                W.mul_(A)
+                bias.add_(b)
+
+                stack[-1].weight.data.copy_(W)
+                if stack[-1].bias is None:
+                    stack[-1].bias = torch.nn.Parameter(bias)
+                else:
+                    stack[-1].bias.data.copy_(bias)
+
+        else:
+            stack.append(m)
+
+    return nn.Sequential(*stack)
--- test_convert_inference.py
+++ test_convert_inference.py
@@ -1,7 +1,43 @@
-import torchvision
 import torch
-import argparse
+from torch.nn import functional as F
+import torchvision.models as models
+from torchvision import transforms
 
+import argparse
+from PIL import Image
+
+from bn_fusion import fuse_bn_sequential
+import time
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='vgg16_bn')
+    args = parser.parse_args()
+
+    trf = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+           ])
+
+    img = trf(Image.open('dog.jpg')).unsqueeze(0)
+
+    net = getattr(models, args.model)(pretrained=True)
+    net.eval()
+
+    start = time.time()
+    with torch.no_grad():
+        res_0 = F.softmax(net(img), 1)
+
+    print('Non fused takes', time.time()-start, 'seconds')
+
+    net.features = fuse_bn_sequential(net.features)
+    start = time.time()
+    with torch.no_grad():
+        res_1 = F.softmax(net(img), 1)
+    print('Fused takes', time.time()-start, 'seconds')
+
+    diff = res_0 - res_1
+    print('L2 Norm of the element-wise difference:', diff.norm().item())
+
+
